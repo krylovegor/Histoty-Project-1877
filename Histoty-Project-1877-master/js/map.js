68c6e4db2c0ba64a9e18f69b8b995897bce641bd
krylovegor <krylov.egor2013@yandex.ru>
2017-02-15 10:23:29 +0300

    Add files via upload

--- Histoty-Project-1877-master/js/map.js
+++ Histoty-Project-1877-master/js/map.js
@@ -1,49 +1,109 @@
-ymaps.ready(function(){
-		var myMap = new ymaps.Map("map", {
-        center: [52.129029, 19.393702],
-        zoom: 1
-    })
-var myPlacemark = new ymaps.Placemark(
-			
-				[42.749289,  25.322719] , {
-                    
-                    hintContent: 'Шипкинский перевал'
-                });     
- 
- 
-			myMap.geoObjects.add(myPlacemark);
-			var myPlacemark1 = new ymaps.Placemark(
-				[51.108249, 17.026901] , {
-                    hintContent: 'Вроцлав'
-                });     
- 
-			myMap.geoObjects.add(myPlacemark1);
-			var myPlacemark2 = new ymaps.Placemark(
-				[52.235576, 21.010265] , {
-                    
-                    hintContent: 'Варшава'
-                });     
- 
-			myMap.geoObjects.add(myPlacemark2);
-			
-// Создание метки 
-var myPlacemark5= new YMaps.Placemark(
-      // Координаты метки
-      new YMaps.GeoPoint(37.6, 55.8), {
-        /* Опции метки:
-           - флаг перетаскивания метки */
-        draggable: true,
-        /* - показывать значок метки 
-             при открытии балуна */
-        hideIcon: false
-      }
-    );
+ymaps.ready(init);
 
-// Задание контента значка метки
-myPlacemark5.name = "Москва";
-// Задание контента балуна
-myPlacemark5.description = "Столица России";
+function init() {
+    var myMap = new ymaps.Map("map", {
+            center: [55.76, 37.64],
+            zoom: 10
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
 
-// Добавление метки на карту
-myMap.addOverlay(myPlacemark5); 
-	});
+    // Создаем геообъект с типом геометрии "Точка".
+        myGeoObject = new ymaps.GeoObject({
+            // Описание геометрии.
+            geometry: {
+                type: "Point",
+                coordinates: [55.8, 37.8]
+            },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                iconContent: 'Я тащусь',
+                hintContent: 'Ну давай уже тащи'
+            }
+        }, {
+            // Опции.
+            // Иконка метки будет растягиваться под размер ее содержимого.
+            preset: 'islands#blackStretchyIcon',
+            // Метку можно перемещать.
+            draggable: true
+        }),
+        myPieChart = new ymaps.Placemark([
+            55.847, 37.6
+        ], {
+            // Данные для построения диаграммы.
+            data: [
+                {weight: 8, color: '#0E4779'},
+                {weight: 6, color: '#1E98FF'},
+                {weight: 4, color: '#82CDFF'}
+            ],
+            iconCaption: "Диаграмма"
+        }, {
+            // Зададим произвольный макет метки.
+            iconLayout: 'default#pieChart',
+            // Радиус диаграммы в пикселях.
+            iconPieChartRadius: 30,
+            // Радиус центральной части макета.
+            iconPieChartCoreRadius: 10,
+            // Стиль заливки центральной части.
+            iconPieChartCoreFillStyle: '#ffffff',
+            // Cтиль линий-разделителей секторов и внешней обводки диаграммы.
+            iconPieChartStrokeStyle: '#ffffff',
+            // Ширина линий-разделителей секторов и внешней обводки диаграммы.
+            iconPieChartStrokeWidth: 3,
+            // Максимальная ширина подписи метки.
+            iconPieChartCaptionMaxWidth: 200
+        });
+
+    myMap.geoObjects
+        .add(myGeoObject)
+        .add(myPieChart)
+        .add(new ymaps.Placemark([55.684758, 37.738521], {
+            balloonContent: 'цвет <strong>воды пляжа бонди</strong>'
+        }, {
+            preset: 'islands#icon',
+            iconColor: '#0095b6'
+        }))
+        .add(new ymaps.Placemark([55.833436, 37.715175], {
+            balloonContent: '<strong>серобуромалиновый</strong> цвет'
+        }, {
+            preset: 'islands#dotIcon',
+            iconColor: '#735184'
+        }))
+        .add(new ymaps.Placemark([55.687086, 37.529789], {
+            balloonContent: 'цвет <strong>влюбленной жабы</strong>'
+        }, {
+            preset: 'islands#circleIcon',
+            iconColor: '#3caa3c'
+        }))
+        .add(new ymaps.Placemark([55.782392, 37.614924], {
+            balloonContent: 'цвет <strong>детской неожиданности</strong>'
+        }, {
+            preset: 'islands#circleDotIcon',
+            iconColor: 'yellow'
+        }))
+        .add(new ymaps.Placemark([55.642063, 37.656123], {
+            balloonContent: 'цвет <strong>красный</strong>'
+        }, {
+            preset: 'islands#redSportIcon'
+        }))
+        .add(new ymaps.Placemark([55.826479, 37.487208], {
+            balloonContent: 'цвет <strong>фэйсбука</strong>'
+        }, {
+            preset: 'islands#governmentCircleIcon',
+            iconColor: '#3b5998'
+        }))
+        .add(new ymaps.Placemark([55.694843, 37.435023], {
+            balloonContent: 'цвет <strong>носика Гены</strong>',
+            iconCaption: 'Очень длиннный, но невероятно интересный текст'
+        }, {
+            preset: 'islands#greenDotIconWithCaption'
+        }))
+        .add(new ymaps.Placemark([55.790139, 37.814052], {
+            balloonContent: 'цвет <strong>голубой</strong>',
+            iconCaption: 'Очень длиннный, но невероятно интересный текст'
+        }, {
+            preset: 'islands#blueCircleDotIconWithCaption',
+            iconCaptionMaxWidth: '50'
+        }));
+}
